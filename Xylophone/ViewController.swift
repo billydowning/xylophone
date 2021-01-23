ebf3c9a2d7c43e5a6d0583d6327d287c308f4359
Billy Downing <wldowning@gmail.com>
2021-01-23 14:32:38 -0700

    changed alpha on button press

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -20,6 +20,13 @@ class ViewController: UIViewController {
     @IBAction func keyPressed(_ sender: UIButton) {
         
         playSound(soundName: sender.currentTitle!)
+        
+        sender.alpha = 0.5
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            
+            sender.alpha = 1.0
+        }
     }
     
     func playSound(soundName: String) {
